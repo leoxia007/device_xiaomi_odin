0f9b773781b2f8d8ac6b0016acf3ebac94732e4b
Pranav Vashi <neobuddy89@gmail.com>
2022-02-24 01:20:27 +0530

    odin: Enable UDFPS icons and animations
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    # Conflicts:
    #	device.mk

--- device.mk
+++ device.mk
@@ -561,6 +561,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# FOD
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
